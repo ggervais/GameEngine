6af5d4212c5321eefbd5eb0bd9e99bc445013c7e
Guillaume Gervais <ggervais@numenor.(none)>
2011-10-21 22:31:55 -0400

    Adjusted Collision object for scenegraph. Nothing else has been coded, though.

--- src/com/ggervais/gameengine/physics/collision/Collision.java
+++ src/com/ggervais/gameengine/physics/collision/Collision.java
@@ -2,15 +2,41 @@ package com.ggervais.gameengine.physics.collision;
 
 import com.ggervais.gameengine.math.Vector3D;
 import com.ggervais.gameengine.Entity;
+import com.ggervais.gameengine.scene.scenegraph.Spatial;
 
 public class Collision {
-	private Entity first;
-	private Entity second;
+
+	private Spatial first;
+	private Spatial second;
 	Vector3D penetrationVector;
 	
-	public Collision(Entity first, Entity second, Vector3D vector) {
+	public Collision(Spatial first, Spatial second, Vector3D penetrationVector) {
 		this.first = first;
 		this.second = second;
-		this.penetrationVector = vector;
+		this.penetrationVector = penetrationVector;
 	}
+
+    public Spatial getFirst() {
+        return first;
+    }
+
+    public void setFirst(Spatial first) {
+        this.first = first;
+    }
+
+    public Spatial getSecond() {
+        return second;
+    }
+
+    public void setSecond(Spatial second) {
+        this.second = second;
+    }
+
+    public Vector3D getPenetrationVector() {
+        return penetrationVector;
+    }
+
+    public void setPenetrationVector(Vector3D penetrationVector) {
+        this.penetrationVector = penetrationVector;
+    }
 }
