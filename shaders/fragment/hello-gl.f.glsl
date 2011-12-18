7b4952cf1f6737f281126c56a7b6763adec4a66a
Guillaume Gervais <ggervais@numenor.localdomain>
2011-12-17 19:36:30 -0500

    Better Phong shading model, but I think it's not perfect.

--- shaders/fragment/hello-gl.f.glsl
+++ shaders/fragment/hello-gl.f.glsl
@@ -8,31 +8,38 @@ varying vec2 fragTexCoords;
 varying vec3 fragNormal;
 varying vec3 fragPosition;
 varying vec4 fragSpecular;
+varying vec3 fragModelViewLightDirection;
 varying float fragShininess;
 
 //const vec3 lightDirection = vec3(0.408248, -0.816497, -0.408248);
-const vec3 lightDirection = vec3(0.0, 0.0, -1);
 const vec4 lightDiffuse = vec4(0.8, 0.8, 0.8, 0.0);
 const vec4 lightAmbient = vec4(0.5, 0.5, 0.5, 1.0);
 const vec4 lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
+float cel(float d)
+{
+    return smoothstep(0.35, 0.37, d) * 0.4 + smoothstep(0.70, 0.72, d) * 0.6;
+}
+
+float warpDiffuse(float d)
+{
+    return cel(d * 0.5 + 0.5);
+}
+
 void main()
 {
-
-    vec3 modelViewLightDirection = (modelViewMatrix * vec4(lightDirection, 0)).xyz;
+    vec3 lightDir = normalize(fragModelViewLightDirection);
     vec3 normal = normalize(fragNormal);
     vec3 eye = normalize(fragPosition);
-    vec3 reflection = reflect(modelViewLightDirection, normal);
+    vec3 reflection = reflect(lightDir, normal);
 
     vec4 color = gl_Color;
 
     vec4 textureColor = texture2D(texture, fragTexCoords);
 
     vec4 fragDiffuse = textureColor * color;
-
-    vec4 diffuseFactor = max(-dot(normal, modelViewLightDirection), 0.0) * lightDiffuse;
+    vec4 diffuseFactor = max(-dot(normal, lightDir), 0.0) * lightDiffuse;
     vec4 ambientDiffuseFactor = diffuseFactor + lightAmbient;
-
     vec4 specularFactor = max(pow(-dot(reflection, eye), fragShininess), 0.0) * lightSpecular;
 
     gl_FragColor = specularFactor * fragSpecular + ambientDiffuseFactor * fragDiffuse;
--- shaders/vertex/hello-gl.v.glsl
+++ shaders/vertex/hello-gl.v.glsl
@@ -11,11 +11,19 @@ varying vec3 fragNormal;
 varying vec3 fragPosition;
 varying vec4 fragSpecular;
 varying float fragShininess;
+varying vec3 fragModelViewLightDirection;
+
+const vec4 spotPosition = vec4(0.0, 0.0, 0.0, 10000.0);
 
 void main(void) {
+
     vec4 eyePosition = modelViewMatrix * position;
     vec4 specular = vec4(1.0, 1.0, 0.75, 0.0);
     float shininess = 4.0;
+
+    vec3 lightDir = normalize(eyePosition.xyz - spotPosition.xyz);
+
+    fragModelViewLightDirection = lightDir;
 
     fragNormal = (modelViewMatrix * vec4(normal, 0)).xyz;
     fragTexCoords = texCoords;
